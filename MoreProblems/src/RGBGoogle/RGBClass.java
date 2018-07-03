862e4b2db9e4996737bcbc23136e5e5ece27d767
Arth Desai <33043106+asd150@users.noreply.github.com>
2018-07-03 15:53:59 -0400

    Initial Commit.

---
+++ MoreProblems/src/RGBGoogle/RGBClass.java
@@ -0,0 +1,53 @@
+package RGBGoogle;
+
+import java.util.ArrayList;
+
+/**
+ * Given an array of strictly the characters 'R', 'G', and 'B', segregate the values of the array so that all the Rs
+ * come first, the Gs come second, and the Bs come last. You can only swap elements of the array.
+ *
+ * Do this in linear time and in-place.
+ *
+ * For example, given the array ['G', 'B', 'R', 'R', 'B', 'R', 'G'],
+ * it should become ['R', 'R', 'R', 'G', 'G', 'B', 'B']..*/
+
+
+public class RGBClass {
+
+    public String[] RGBSwap(String[] array){
+
+        ArrayList<String> red = new ArrayList<>();
+        ArrayList<String> blue = new ArrayList<>();
+        ArrayList<String> green = new ArrayList<>();
+        ArrayList<String> answer = new ArrayList<>();
+        int redI =0,blueI =0,greenI =0;
+
+        for(int i=0;i<array.length;i++){
+                if(array[i].equals("R")){
+                    red.add(redI,"R");
+                    redI++;
+                }
+                else if(array[i].equals("B")){
+                    blue.add(blueI,"B");
+                    blueI++;
+                }
+                else
+                {
+                    green.add(greenI,"G");
+                    greenI++;
+                }
+        }
+
+        answer.addAll(red);
+        answer.addAll(blue);
+        answer.addAll(green);
+        answer.toArray();
+
+        for(int i=0;i<answer.toArray().length;i++){
+            System.out.print(answer.toArray()[i]);
+        }
+
+
+        return array;
+    }
+}
---
+++ MoreProblems/src/RGBGoogle/RGBMain.java
@@ -0,0 +1,10 @@
+package RGBGoogle;
+
+public class RGBMain {
+    public static void main(String[] args){
+        RGBClass rgbClass = new RGBClass();
+        String[] array = {"G", "B", "R", "R", "B", "R", "G"};
+        rgbClass.RGBSwap(array);
+
+    }
+}
